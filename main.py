89efd2c94c7aa87765cc0376267b456ca7f735fd
-- <-->
2025-01-23 15:33:28 -0600

    project setup

---
+++ main.py
@@ -0,0 +1,8 @@
+"""
+Quiz game where user answers a number of multiple choice and/or true and false questions.
+Following the quiz their score is displayed.
+"""
+
+
+if __name__ == "__main__":
+    pass
